be2a5124111a35b90ff5bf19e099aeb1edd270cc
SergioAnd18 <developinc@yandex.ru>
2017-01-21 12:07:58 +0200

    fix order form and plus-minus

--- apps/order/forms.py
+++ apps/order/forms.py
@@ -5,7 +5,7 @@ from .models import SimpleOrder
 
 
 class SimpleOrderForm(forms.ModelForm):
-    phone = phone_number = forms.RegexField(widget=forms.TextInput(attrs={'placeholder': u'Ваш телефон'}),
+    phone = forms.RegexField(widget=forms.TextInput(attrs={'placeholder': u'Ваш телефон'}),
                                             regex=r'^\+?1?\d{9,15}$',
                                             error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
     class Meta:

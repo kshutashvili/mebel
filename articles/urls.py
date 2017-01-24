d3f695ad3e6c3744e781ae3301986d3a94a33e56
SergioAnd18 <developinc@yandex.ru>
2017-01-24 12:55:45 +0200

    MJ-19

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from articles.views import ArticleListView
+from articles.views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     url(r'^$', ArticleListView.as_view(), name='all'),
+    url(r'^(?P<pk>[\d]+)', ArticleDetailView.as_view(), name='detail')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 # Create your views here.
@@ -10,3 +10,9 @@ class ArticleListView(ListView):
     model = Article
     template_name = 'articles/all_articles.html'
     context_object_name = 'articles_list'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/article_detail.html'
+    context_object_name = 'article'

689cdf381b65905e0a8ec997ae1f3e4c3b5b64fe
Dmitry-Kiselev <kdem27@gmail.com>
2017-04-01 21:41:35 +0300

    Revert "[MJ-44] enable staticfiles minification"
    This reverts commit 994efe93e186547bcca8c77e515695074fef2c01.
    Errors may happen if CDN resources in compress block, so we
    shouldn't add compress tag globally

--- mebeljazz/settings.py
+++ mebeljazz/settings.py
@@ -172,15 +172,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # other finders..
-    'compressor.finders.CompressorFinder',
-)
-
-COMPRESS_ENABLED = True
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

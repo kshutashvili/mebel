87270c9308255c8afee247978fb1ef38581fa841
Kate <kate.glebova97@gmail.com>
2017-09-09 01:07:15 +0300

    MJ-77 Recommended slider

--- apps/promotions/views.py
+++ apps/promotions/views.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 from django.core.paginator import Paginator
+from oscar.apps.promotions.models import HandPickedProductList
 
 from oscar.apps.promotions.views import HomeView as DefaultHomeView
 
@@ -18,15 +21,15 @@ class HomeView(DefaultHomeView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['discounted_products'] = StockRecord.objects.filter(product__discount_type__isnull=False)
-        last_products = Product.browsable.base_queryset()
-        paginator = Paginator(last_products, self.paginate_by)
-        page = self.request.GET.get('page')
         try:
-            last_products = paginator.page(page)
-        except PageNotAnInteger:
-            last_products = paginator.page(1)
-        except EmptyPage:
-            last_products = paginator.page(paginator.num_pages)
-        context['last_products'] = last_products
+            context['popular'] = HandPickedProductList.objects.get(name=u'Популярное').get_products()
+        except HandPickedProductList.DoesNotExist:
+            pass
+
+        try:
+            context['recommended'] = HandPickedProductList.objects.get(name=u'Рекомендуемое').get_products()
+        except HandPickedProductList.DoesNotExist:
+            pass
+
         context['slides'] = SliderSlide.objects.all()
         return context
--- static/js/script.js
+++ static/js/script.js
@@ -1,71 +1,35 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider({
-	  minSlides: 1,
-	  maxSlides: 1,
-	  auto: true,
-	  moveSlides: 1,
-	  pause: 3000
+$(document).ready(function () {
+
+	$('.bxslider').bxSlider({
+		minSlides: 1,
+		maxSlides: 1,
+		auto: true,
+		moveSlides: 1,
+		pause: 3000
 	});
 
-  $('.bxslider-catalog').bxSlider({
-      minSlides: 3,
-      maxSlides: 3,
-      slideWidth: 295,
-      slideMargin: 0,
-      nextSelector: '#slider-next',
-      prevSelector: '#slider-prev',
-    });
+	var $sliderBlocks = $(".tovar, .catalog");
 
-  $('.bxslider-popular').bxSlider({
-      minSlides: 3,
-      maxSlides: 3,
-      slideWidth: 295,
-      slideMargin: 0,
-      nextSelector: '#slider-next-p',
-      prevSelector: '#slider-prev-p',
-    });
 
-  $('#myTabs a').click(function (e) {
-	  e.preventDefault()
-	  $(this).tab('show')
+	$sliderBlocks.each(function () {
+		var $slider = $(this).find(".slider");
+		var $left = $(this).find("a.left");
+		var $right = $(this).find("a.right");
+		$slider.bxSlider({
+			pager: false,
+			nextSelector: $right,
+			prevSelector: $left,
+			slideWidth: 295,
+			minSlides: 2,
+			maxSlides: 3,
+			moveSlides: 1
+		});
 	});
 
-	 $('.bxslider_two').bxSlider({
-	  pagerCustom: '#bx-pager',
-	  minSlides: 1,
-	  maxSlides: 1,
-	  moveSlides: 1
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
 	});
 
-	$('.owl-carousel').owlCarousel({
-		items:3,
-	    margin:10,
-	    nav:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:2
-	        },
-	        1000:{
-	            items:3
-	        }
-	    }
-	})
 
-	 $(".search_button").click(function (){
-		 if($(".dis_inp").hasClass('active')){
-			 $(".dis_inp").css("display", "none");
-			 $(".constructor").css("display", "inline-block");
-			 $(".dis_inp").removeClass('active');
-		 }
-		 else{
-			 $(".dis_inp").css("display", "inline-block");
-			 $(".dis_inp").addClass('active');
-			 $(".constructor").css("display", "none");
-		 }
-
-
-  	});
 });
